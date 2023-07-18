fa118076f86c37a21ba17ab923777123cace5fb3
blackpearlhn <blackpearlhn13@gmail.com>
2023-07-18 23:22:35 +0530

    file updated

--- birthday/birthdayfiles/config.js
+++ birthday/birthdayfiles/config.js
@@ -1,6 +1,5 @@
 var config = {
-    // The length of the sentence can be arbitrary, you can write ten sentences, twenty sentences are fine
-    // Try not to exceed 15 characters per sentence, otherwise the display effect may not be very good
+    
     texts: [
         'Congratulations on your day, dear friend.',
         'May this birthday be tremendous,',
@@ -10,20 +9,11 @@ var config = {
         'Happy birthday to my beautiful friend!',
         'Enjoy your special day to the fullest, bestie!'
     ],
-    /**
-     * imgs can be left blank, but if you want to fill in, you must follow the format below
-     * "Corresponding to the above text, it must be exactly the same" : "The image address, you can put the image in the imgs folder"
-     * For example
-     * "Beloved little cutie": "./imgs/xiaokeai.jpg"
-     *
-     * If you don't want pictures, just write two slash comments at the beginning of each line, for example, the picture "Today is your birthday" below will not be displayed :)
-     * Tip: It is better to use a square or close to a square for the picture, it looks better
-     */
+
     imgs: {
         "Dear little cutie": "birthdayfiles/imgs/xiaokeai.png",
         // "Today is your birthday": "birthdayfiles/imgs/birthday.jpg",
     },
-    // button text description, the following is the default button text, in English, you can change it to your favorite text
     desc: {
         turn_on: "Start",
         play: "Music",
@@ -33,6 +23,6 @@ var config = {
         light_candle: "Candle?",
         wish_message: "Happy Birthday",
         story: "A MESSAGE FOR YOU",
-        lst_greet:"Happy Birthday!",
+        lst_greet:"Happy Birthday! Aaliya (Aalu🥔)",
     }
 };
