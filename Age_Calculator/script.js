00f41251a66b1bf512294c2bfc1ad0b2cc3cd6dd
blackpearlhn <blackpearlhn13@gmail.com>
2023-10-02 16:11:23 +0530

    new file added 2-10-23

--- Age_Calculator/script.js
+++ Age_Calculator/script.js
@@ -1,5 +1,4 @@
-const age = () =>
-{
+const age = () => {
     let d1 = document.getElementById('date').value
     let m1 = document.getElementById('month').value
     let y1 = document.getElementById('year').value
