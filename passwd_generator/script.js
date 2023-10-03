41471123cbd0286164c5405e617a84560716521b
blackpearlhn <blackpearlhn13@gmail.com>
2023-10-03 17:06:38 +0530

    files updated 2-10-23

--- passwd_generator/script.js
+++ passwd_generator/script.js
@@ -1,22 +1,20 @@
-let len = document.getElementById("psdlength");
-let pass = document.getElementById("pass");
-let submit = document.getElementById("submit");
-len = parseInt(len.value)
-
 const passh = [  "a",  "b",  "c",  "d",  "e",  "f",  "g",  "h",  "i",  "j",  "k",  "l",  "m",  "n",  "o",  "p",  "q",  "r",  "s",  "t",  "u",  "v",  "w",  "x",  "y",  "z",  "A",  "B",  "C",  "D",  "E",  "F",  "G",  "H",  "I",  "J",  "K",  "L",  "M",  "N",  "O",  "P",  "Q",  "R",  "S",  "T",  "U",  "V",  "W",  "X",  "Y",  "Z",  "1",  "2",  "3",  "4",  "5",  "6",  "7",  "8",  "9",  "0",  " ",  "!",  "@",  "#",  "$",  "%",  "^",  "&",  "*",  "+",  "-",  "/",  "`",  "~",  "£",  "€",  "(",  ")",  "|",  "\\", "'",  '"',  "=",  ",",  ".",  "{",  "}",  "[",  "]",  "?",];
 
-const generate_pwd = () => {
-        password = "";
-        for (let i = 0; i < len; i++) {
-            password += passh[Math.floor(Math.random() * passh.length)];
-        }
-            pass.innerHTML = "You're New Password is: " + password;
-            console.log("You're New Password is: ", password);
+
+document
+    .getElementById("generatePassword")
+    .addEventListener("click", generatePassword);
+
+function generatePassword() {
+    const passwordLength = parseInt(
+    document.getElementById("passwordLength").value
+    );
+
+    let password = "";
+    for (let i = 0; i < passwordLength; i++) {
+    const randomIndex = Math.floor(Math.random() * passh.length);
+    password += passh[randomIndex];
     }
 
-
-const submit_func = (event) => {
-    event.preventDefault()
+    document.getElementById("password").textContent = password;
 }
-
-submit.addEventListener('click', submit_func)
