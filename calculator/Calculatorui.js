300c3c8012a111c2d69bf44075ae0ad05734bd52
sayedhanzala <sayedhanzala123@gmail.com>
2023-11-28 13:49:48 +0530

    calcy updated

--- calculator/Calculatorui.js
+++ calculator/Calculatorui.js
@@ -1,18 +1,29 @@
-const btnE1 = document.querySelectorAll("button");
-const resultE1 = document.getElementById("inputbox");
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
+const specialChars = ["%", "*", "/", "-", "+", "="];
+let output = "";
 
-for (let i = 0; i < btnE1.length; i++) {
-  btnE1[i].addEventListener("click", () => {
-    const btnValue = btnE1[i].textContent;
-    console.log(btnValue);
-    if (btnValue === "c") {
-      resultE1.value = "";
-    } else if (btnValue === "=") {
-      resultE1.value = eval(resultE1.value);
-    } else if (btnValue === "*") {
-      resultE1.value = resultE1.value = btnValue;
-    } else {
-      resultE1.value = resultE1.value + btnValue;
-    }
-  });
-}
+//Define function to calculate based on button clicked.
+const calculate = (btnValue) => {
+  display.focus();
+  if (btnValue === "=" && output !== "") {
+    //If output has '%', replace with '/100' before evaluating.
+    output = eval(output.replace("%", "/100"));
+  } else if (btnValue === "AC") {
+    output = "";
+  } else if (btnValue === "DEL") {
+    //If DEL button is clicked, remove the last character from the output.
+    output = output.toString().slice(0, -1);
+  } else {
+    //If output is empty and button is specialChars then return
+    if (output === "" && specialChars.includes(btnValue)) return;
+    output += btnValue;
+  }
+  display.value = output;
+};
+
+//Add event listener to buttons, call calculate() on click.
+buttons.forEach((button) => {
+  //Button click listener calls calculate() with dataset value as argument.
+  button.addEventListener("click", (e) => calculate(e.target.dataset.value));
+});
